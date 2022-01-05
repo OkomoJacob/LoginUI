0bd825916c1c39c8e6f95dacd7e211eaffa9ccd1
okomojacob <jaylabs2030@gmail.com>
2022-01-05 15:40:44 +0300

    Login Screen, with Login :key:

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -19,45 +19,65 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     //Email Field
     final emailField = TextFormField(
-      autofocus: false,
-      controller: emailController,
-      keyboardType: TextInputType.emailAddress,
-      // validator: (){},
-      onSaved: (value) {
-        emailController.text = value!;
-      },
-      textInputAction: TextInputAction.next,
-      // Styling Email Field
-      decoration: InputDecoration(
-        // ignore: unnecessary_const
-        prefixIcon: Icon(Icons.mail),
-        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-        hintText: "Email",
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-      ));
-  
+        autofocus: false,
+        controller: emailController,
+        keyboardType: TextInputType.emailAddress,
+        // validator: (){},
+        onSaved: (value) {
+          emailController.text = value!;
+        },
+        textInputAction: TextInputAction.next,
+        // Styling Email Field
+        decoration: InputDecoration(
+          // ignore: unnecessary_const
+          prefixIcon: Icon(Icons.mail),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Email",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+
     //Password Field
     final passwordField = TextFormField(
-      autofocus: false,
-      controller: passwordController,
-      // validator: (){},
-      onSaved: (value) {
-        passwordController.text = value!;
-      },
-      textInputAction: TextInputAction.done,
+        autofocus: false,
+        controller: passwordController,
+        obscureText: true,
+        // validator: (){},
+        onSaved: (value) {
+          passwordController.text = value!;
+        },
+        textInputAction: TextInputAction.done,
 
-    // Styling Password Field
-      decoration: InputDecoration(
-        // ignore: unnecessary_const
-        prefixIcon: Icon(Icons.lock),
-        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-        hintText: "Enter Password",
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-      ));
+        // Styling Password Field
+        decoration: InputDecoration(
+          // ignore: unnecessary_const
+          prefixIcon: Icon(Icons.lock),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Enter Password",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+
+    // Button
+    final loginButton = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(20),
+      color: Colors.blue,
+      child: MaterialButton(
+        padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        minWidth: MediaQuery.of(context).size.width,
+        onPressed: (){},
+        child: Text(
+          "Login",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold,
+          ),
+          ),
+      ),
+    );
 
     return Scaffold(
       backgroundColor: Colors.white,
@@ -68,14 +88,11 @@ class _LoginScreenState extends State<LoginScreen> {
             child: Form(
               key: _formKey,
               child: Column(
-                children: <Widget>[
-                  emailField,
-                  passwordField,
-                ],
+                children: <Widget>[emailField, passwordField, loginButton],
               ),
             ),
           ),
-        ), 
+        ),
       ),
     );
   }
